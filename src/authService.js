701b42c05a059a43cf276d74500900b43974dd1f
KarlapudiPrashanthi <prashanthikarlapudi7@gmail.com>
2025-11-28 02:17:06 +0530

    Update API_URL to new authentication endpoint

--- src/authService.js
+++ src/authService.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = "http://3.236.81.110:8080/auth";
+const API_URL = "http://44.220.64.228:8080/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
